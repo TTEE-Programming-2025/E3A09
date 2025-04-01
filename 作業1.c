2a99d1e85a1cb35be6bbd6d33701158eef525aa8
E3A09 <u11102109@o365.ttu.edu.tw.com>
2025-04-02 05:52:09 +0800

    first commit

---
+++ 作業1.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	printf("************************\n");
+	printf("-----------------\n");
+	system("pause");
+	system("CLS");
+	
+	
+	int password;
+	printf("¿é¤J4¦ì¼Æ¦r±K½X\n");
+	scanf("%d",&password);
+	
+	if(password==2024){
+		printf("Åwªï¡I\n");
+		system("pause");
+	}
+	else{
+		printf("±K½X¿ù»~-.-\n");
+		system("pause");
+		return 0;
+	}
+	system("CLS");
+	
+	
+	
+}
+
