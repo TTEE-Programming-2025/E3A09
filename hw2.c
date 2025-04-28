675520c4934fe790b2dfed988bbca0bc91ea4d29
E3A09 <end930216@gmail.com>
2025-04-28 22:23:03 +0800

    first commit

--- hw2.c
+++ hw2.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
 int main(){
 	printf("************************\n");
 	printf("-----------------\n");
@@ -26,28 +27,44 @@ int main(){
 	system("CLS");
 	
 	
+	int i=0;
 	int password;
-	int i;
-	printf("輸入4位數字密碼\n");
-	scanf("%d",&password);
-	
-	if(password==2025){
-		printf("歡迎！\n");
-		system("pause");
-	}
+		
+	while (i < 3) {
+		printf("請輸入4位數密碼(2025)\n");
+		scanf("%d",&password);
+			
+    if (password == 2025) {
+        printf("密碼正確\n");
+        break;  
+    } 
 	else{
-		for(i=1;i<3;i++){
-				printf("密碼錯誤-.-\n");
-				printf("輸入4位數字密碼\n");
-	            scanf("%d",&password);
-		    }
-			printf("密碼錯誤3次-.-\n\a");
-			system("pause");
-			return 0;	
+           i++;
+           printf("密碼錯誤\n");
+        }
+    if (i == 3) {
+        printf("錯誤次數達到上限，程式結束\n\a");
+        return 0;
 	}
-	system("CLS");
+}
+	system("CLS");	
+		
 	
+	char a,A,b,B,c,C;
+	printf("************************\n");
+	printf("| a. 畫出直角三角形 |   \n");
+	printf("|                   |   \n");
+	printf("| b. 顯示乘法表     |   \n");
+	printf("|                   |   \n");
+	printf("| c. 結束           |   \n");
+	printf("************************\n");
+	
+	printf("請選擇:\n");
+	scanf("%c",&a,&A,&b,&B,&c,&C);
+	system("pause");
+	return 0;
 	
 	
 }
+
 
